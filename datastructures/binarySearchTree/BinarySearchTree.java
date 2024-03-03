2dbbf3e703083c63a550eb6205a8be16e4ba3717
bgwozdz <bartoszgwozdz@gmail.com>
2024-03-03 19:45:36 +0100

    Implement BFS tree traversal

--- datastructures/binarySearchTree/BinarySearchTree.java
+++ datastructures/binarySearchTree/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package datastructures.binarySearchTree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinarySearchTree {
     public Node root;
 
@@ -30,6 +35,24 @@ public class BinarySearchTree {
             }
         }
         return false;
+    }
+
+    public ArrayList<Integer> BFS() {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(currentNode);
+        while(queue.size() > 0){
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+            if(currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+        return results;
     }
 
     public boolean contains(int value){
